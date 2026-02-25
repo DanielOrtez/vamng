a70e738268f3191ebd2c46c8ec5429867b7f9837
Daniel Ortez <93644152+DanielOrtez@users.noreply.github.com>
2026-02-25 17:48:11 +0100

    login and register templates moved from users to users/auth

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import redirect, render
 
 from .forms import LoginForm, RegisterForm
@@ -18,7 +18,7 @@ def register(request):
     else:
         register_form = RegisterForm()
 
-    return render(request, "users/register.html", {"form": register_form})
+    return render(request, "users/auth/register.html", {"form": register_form})
 
 
 @user_passes_test(not_logged_in, login_url="/", redirect_field_name=None)
@@ -30,4 +30,4 @@ def login(request):
             auth_login(request, login_form.get_user())
             return redirect("home")
 
-    return render(request, "users/login.html", {"form": login_form})
+    return render(request, "users/auth/login.html", {"form": login_form})
